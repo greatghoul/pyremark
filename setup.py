6bbb076d097726cfea0867696641ef681820122d
greatghoul <greatghoul@gmail.com>
2013-04-17 21:25:31 +0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name = "pyremark",
+    version = "0.1.0",
+    author = "greatghoul",
+    author_email = "greatghoul+pyremark@gmail.com",
+    description = ("Remark slides utilities"),
+    install_requires = ['pyinotify'],
+    platforms='any',
+    license = "MIT",
+    url = "http://www.g2w.me/tag/pyremark/",
+    # packages=['pyremark'],
+    scripts = ['remark.py']
+)
